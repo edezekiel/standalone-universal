62798248cf248060a7f5fa9eec592088f5d3d9b4
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2023-04-13 08:12:38 -0500

    Fix error in generated server.ts

--- server.ts
+++ server.ts
@@ -5,7 +5,7 @@ import { ngExpressEngine } from '@nguniversal/express-engine';
 import * as express from 'express';
 import { existsSync } from 'node:fs';
 import { join } from 'node:path';
-import bootstrap from './src/main.server;
+import bootstrap from './src/main.server'
 
 // The Express app is exported so that it can be used by serverless Functions.
 export function app(): express.Express {
